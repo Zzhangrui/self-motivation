e651d31df5f0cc64401f78f9122feaa49bfafd29
zhangrui <zhangruiaq@gmail.com>
2020-06-25 00:43:52 +0800

    初始化

---
+++ src/test/java/com/yangq/selfmotivation/SelfMotivationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yangq.selfmotivation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfMotivationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
